f446ee86039af01b7574141cd70f334f9888d5eb
annabessant <anna@thefutureofinnovation.org>
2023-01-24 13:46:46 +0000

    add write and save finctions

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ $(document).ready(function () {
     let day = moment().format("dddd"); // for showing day of week.
     $("#currentDay").html(day + ", " + date);
 
+    $('.saveBtn').on('click', savePlan);
+    function savePlan() {
+        // to be completed 
+    let plan = $(this).siblings('.description').val();
+    let hour = $(this).parent().attr('id');
 
+    // save in localStorage
+    localStorage.setItem(hour, plan);
+    } 
 
 });
