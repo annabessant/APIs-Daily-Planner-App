9b3bec04e95de8d1721159623deedc625c233829
annabessant <anna@thefutureofinnovation.org>
2023-01-24 13:55:30 +0000

    add colour coding

--- script.js
+++ script.js
@@ -13,4 +13,32 @@ $(document).ready(function () {
     localStorage.setItem(hour, plan);
     } 
 
+    // let plan = $(this).siblings('.description').val();
+    // let hour = $(this).parent().attr('id');
+
+    // // save in localStorage
+    // localStorage.setItem(hour, plan);
+    function colorCodePlans() {
+    // get current number of hours
+    let currentTime =  moment().format("H");
+    console.log("currentTime = ", currentTime);
+
+    $('.time-block').each(function () {
+        let eventTime = parseInt($(this).attr('id').split('-')[1]);
+  
+        // check if we've moved past this time
+        if (eventTime < currentTime) {
+          $(this).addClass('past');
+        } else if (eventTime === currentTime) {
+          $(this).removeClass('past');
+          $(this).addClass('present');
+        } else {
+          $(this).removeClass('past');
+          $(this).removeClass('present');
+          $(this).addClass('future');
+        }
+      });
+    }
+
+    colorCodePlans();
 });
